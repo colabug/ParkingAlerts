bab60727d47519ff28d0c9902cb205cdc2dca6ef
Corey Leigh Latislaw <colabug@gmail.com>
2011-07-25 21:54:33 -0400

    Added function to translate a point to an address.

--- src/com/greenlife/parking/BaseOverlay.java
+++ src/com/greenlife/parking/BaseOverlay.java
@@ -2,6 +2,8 @@ package com.greenlife.parking;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.view.MotionEvent;
 import android.widget.Toast;
 
@@ -9,6 +11,10 @@ import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 abstract public class BaseOverlay extends ItemizedOverlay
 {
@@ -26,17 +32,62 @@ abstract public class BaseOverlay extends ItemizedOverlay
     @Override
     public boolean onTouchEvent( MotionEvent event, MapView mapView )
     {
+        // Touch and release of a point
         if ( event.getAction() == MotionEvent.ACTION_UP )
         {
+            // Get point clicked
             GeoPoint point = mapView.getProjection()
                                     .fromPixels( (int) event.getX(),
                                                  (int) event.getY() );
-            Toast.makeText( context,
-                            point.getLatitudeE6() / 1E6 + "," +
-                            point.getLongitudeE6() / 1E6,
+
+            // Decode point to an address string
+            String addressString = translatePointToAddress( point );
+            Toast.makeText( getContext(),
+                            addressString,
                             Toast.LENGTH_SHORT ).show();
+            return true;
         }
-        return false;
+        else
+        {
+            return false;
+        }
+    }
+
+    private String translatePointToAddress( GeoPoint point )
+    {
+        String defaultString = "No address found.";
+        Geocoder geoCoder = new Geocoder( getContext(),
+                                          Locale.getDefault() );
+
+        // Attempt to get address(es)
+        List<Address> addresses;
+        try
+        {
+            addresses = geoCoder.getFromLocation( point.getLatitudeE6()  / 1E6,
+                                                  point.getLongitudeE6() / 1E6,
+                                                  1 );
+        }
+        catch ( IOException exception )
+        {
+            // Return default if an error occurred
+            exception.printStackTrace();
+            return defaultString;
+        }
+
+        // Return default if nothing found
+        if ( addresses.size() <= 0 )
+        {
+            return defaultString;
+        }
+
+        // Construct address string
+        String addressString = "";
+        for ( int i = 0; i < addresses.get( 0 ).getMaxAddressLineIndex(); i++ )
+        {
+            addressString += addresses.get( 0 ).getAddressLine( i ) + "\n";
+        }
+
+        return addressString;
     }
 
     public Context getContext()
